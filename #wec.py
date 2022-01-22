926bc8bca4e320c4768a95859f613ec002bbfc29
Wyatt <wyattshenfield@me.com>
2022-01-21 16:02:46 -0800

    algorythm v1.0 working

--- #wec.py
+++ #wec.py
@@ -2,7 +2,7 @@
 from ast import Break
 from threading import setprofile
 
-inp = [3,1,2,1,4,0,3]
+inp = [3,1,2,1,4,0,1]
 out = [2,1,3,1,4,0,1]
 lilstack = []
 bigstack = []
@@ -28,7 +28,7 @@ rem = len(inp)%2
 if rem == 0:
     half = len(inp)/2
 else:
-    half = len(inp)/2 - rem
+    half = (len(inp)-1)/2
 
 for n in range(len(inp)):
     if n > half:
@@ -43,63 +43,78 @@ if sum2 > sum1:
 else: 
     start = bigstack[0]
 print(start)
+print(lilstack,bigstack)
 
 #start moving boxes
 while (len(lilstack) > 0) or (len(bigstack) > 0):
-    if start == craneloc:
-        pass
+
+    if start > craneloc:
+        step = abs(start - craneloc)
+        for n in range(step):
+            process.append(2)
     elif start < craneloc:
-        step = craneloc - start
+        step = abs(craneloc - start)
         for n in range(step):
             process.append(1)
     else:
-        step = start - craneloc
-        for n in range(step):
-            process.append(2)
+        pass 
+    
     craneloc = start
     inp[craneloc] -= 1
     if inp[craneloc] == out[craneloc]:
         bigstack.remove(craneloc)
     else:
         pass
+    
+    process.append(3)
     print(process)
     print(craneloc)
-    process.append(3)
 
     # box in hand
-    step = start - lilstack[len(lilstack)-1]
-    if start < lilstack[len(lilstack)-1]:
-        for n in range(step):
-            process.append(2)
-            craneloc += 1
-    elif start > lilstack[len(lilstack)-1]:
-        for n in range(step):
-            process.append(1)
-            craneloc -=1
-    else:
-        continue
+    print(lilstack)
+    print(bigstack)
+    if len(lilstack) > 0:
+        step = abs(craneloc - lilstack[len(lilstack)-1])
+        print(step) #2
+        if start < lilstack[len(lilstack)-1]:
+            for n in range(step):
+                process.append(2)
+                craneloc += 1
+        elif start > lilstack[len(lilstack)-1]:
+            for n in range(step):
+                process.append(1)
+                craneloc -=1
+        else:
+            continue
 
-    inp[craneloc] += 1
-    if inp[craneloc] == out[craneloc]:
-        lilstack.remove(craneloc)
+        inp[craneloc] += 1
+        if inp[craneloc] == out[craneloc]:
+            lilstack.remove(craneloc)
+        else:
+            pass
     else:
-        pass
+        break
+
     process.append(4)
     print(process)
     print(craneloc)
-    
+
     # once crane has dropped box
-    step = start - bigstack[len(bigstack)-1]
-    if start < bigstack[len(bigstack)-1]:
-        for n in range(step):
-            process.append(2)
-            craneloc += 1 
-    elif start > bigstack[len(bigstack)-1]:
-        for n in range(step):
-            process.append(1)
-            craneloc -= 1
+    if len(bigstack) > 0:
+        step = abs(start - bigstack[len(bigstack)-1])
+        if start < bigstack[len(bigstack)-1]:
+            for n in range(step):
+                process.append(2)
+                craneloc += 1 
+        elif start > bigstack[len(bigstack)-1]:
+            for n in range(step):
+                process.append(1)
+                craneloc -= 1
+        else:
+            continue
     else:
-        continue
+        break
+
 
 
 # compare input and output, set flags for each stack starting from the left (=/</>)
